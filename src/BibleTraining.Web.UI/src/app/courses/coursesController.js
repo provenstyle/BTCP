4bd7631723c31ad3a471d2e9927fad21b020c874
michael dudley <michael.dudley@improvingenterprises.com>
2017-04-24 08:26:15 -0500

    handling row selection

--- src/BibleTraining.Web.UI/src/app/courses/coursesController.js
+++ src/BibleTraining.Web.UI/src/app/courses/coursesController.js
@@ -1,11 +1,8 @@
-﻿//import "./playerFeature.js";
-//import "./createPlayerController.js";
-
-new function() {
+﻿new function() {
 
     bt.package(this, {
         name:    "course",
-        imports: "miruken.mvc,miruken.ng,bt",
+        imports: "miruken.context,miruken.mvc,miruken.ng,bt",
         exports: "CoursesController"
     });
 
@@ -17,8 +14,11 @@ new function() {
             dtOptions: undefined,
             dtColumns: undefined
         },
-        $inject: ["DTOptionsBuilder", "DTColumnBuilder", $appContext],
-        constructor(optionsBuilder, columnBuilder) {
+        $inject: ["DTOptionsBuilder", "DTColumnBuilder", "$scope", Context],
+        constructor(optionsBuilder, columnBuilder, $scope, context) {
+            this.$scope = $scope;
+            this.context = context;
+
             this.dtOptions = optionsBuilder.newOptions()
                 .withOption('ajax', {
                     url: 'api/courses',
@@ -48,18 +48,33 @@ new function() {
                 .withLightColumnFilter({
                     '0' : { html: 'input', type: 'text', attr: { class: 'form-control' }},
                     '1' : { html: 'input', type: 'text', attr: { class: 'form-control' }}
-                });
+                })
+                .withOption('rowCallback', this.rowCallback.bind(this));
 
             this.dtColumns = [
                 columnBuilder.newColumn('name').withTitle('Name'),
                 columnBuilder.newColumn('description').withTitle('Description')
             ];
         },
+        rowCallback(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+            // Unbind first in order to avoid any duplicate handler
+            //(see https://github.com/l-lin/angular-datatables/issues/87)
+            $('td', nRow).unbind('click');
+            $('td', nRow).bind('click', () => {
+                this.$scope.$apply(() => {
+                    this.rowSelected(aData);
+                });
+            });
+            return nRow;
+        },
         showCourses() {
             return ViewRegion(this.io).show("app/courses/courses");
         },
         goToCreate() {
             bt.course.CreateCourseController(this.io).next(ctrl => ctrl.showCreateCourse());
+        },
+        rowSelected(data) {
+           bt.course.CourseController(this.context).next(ctrl => ctrl.showCourse(data));
         }
     });
 
